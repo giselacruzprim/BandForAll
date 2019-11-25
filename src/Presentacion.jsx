6945511832a0e6ae0d1d5ad052412aa35f40eadd
Simonas Valiuska <simonasvaliuska13@gmail.com>
2019-11-25 18:13:52 +0100

    canvi h1 frase

--- src/Presentacion.jsx
+++ src/Presentacion.jsx
@@ -11,7 +11,7 @@ class Presentacion extends React.Component {
           <div className="row">
             <div className="col">
               <h1 className="fraseWeb">
-              Tu banda está aquí, solo te hace falta encontrarla.
+              Tu banda está aquí, solo te hace falta encontrarla. :)
               </h1>
               <h4 className="fraseWeb2"><span>34.000+</span> personas ya lo han hecho.</h4>
             </div>

8290d626d7baa02cca3a09de272af8d04007a274
giselacruzprim <giselacruzprim16@gmail.com>
2019-11-26 18:50:50 +0100

    cambios css + ayuda faqs (gis)

--- src/Ayuda.jsx
+++ src/Ayuda.jsx
@@ -5,8 +5,22 @@ class Ayuda extends React.Component {
   render() {
     return (
       <>
-      
-        <h1>Gánate la vida</h1>
+        <h1 className="faqs">Preguntas frecuentes (FAQ's)</h1>
+
+        <div className="card">
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" className="btn btn-primary">Button</a>
+  </div>
+</div>>
+
+
+
+
+
+
+        <a className="contacto" href="mailto:giselacruzprim16@gmail.com">Contacto </a>
       </>
     );
   }

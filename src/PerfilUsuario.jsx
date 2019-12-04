5d2f11ebf5ee1b488fe895e3f960c913096bd20f
cacaolat123 <marcelmontes00@gmail.com>
2019-12-04 16:44:58 +0100

    perfil usuario

--- src/PerfilUsuario.jsx
+++ src/PerfilUsuario.jsx
@@ -1,45 +1,63 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Jumbotron, Container, Button } from "reactstrap";
-
+import { APIURL} from "./Datos.js";
 
 class perfilUsuario extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      usuarioPerfil: []
+    };
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    fetch(APIURL + `bandforall_musico/${this.props.obtenUsuarioRegistrado()}`)
+      .then(data => data.json())
+      .then(datos =>
+        this.setState({
+          usuarioPerfil: datos[0]
+        })
+      )
+      .catch(err => console.log(err));
+  }
 
   render() {
     return (
       <>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+
         <div className="container-fluid">
           <div className="row">
             <div className="col-3"></div>
             <div className="col-5">
               <Jumbotron fluid>
                 <Container fluid>
-                  <h1 className="display-3">Perfil</h1>
-                  <p className="lead">
-                    This is a modified jumbotron that occupies the entire
-                    horizontal space of its parent.
-                  </p>
+                  <h5 className="lead">{this.state.usuarioPerfil.nombre} {this.state.usuarioPerfil.apellido}</h5>
                 </Container>
               </Jumbotron>
               <div className="row">
-                <h2>Nombre:</h2>  <h2>Apellido:</h2>
                 <div className="col-5"></div>
-                
+
                 <div className="col-7"></div>
               </div>
-
-              <h5>Edad</h5>
-              <h5>Instrumento 1:</h5> <h5>Nivel:</h5>
-              <h5>Instrumento 2:</h5> <h5>Nivel:</h5>
-              <h5>Instrumento 3:</h5> <h5>Nivel:</h5>
+              <h5>Edad: {this.state.usuarioPerfil.edad}</h5>
+              <h5>Instrumento 1: {this.state.usuarioPerfil.instrumento1}</h5> <h5>Nivel: {this.state.usuarioPerfil.nivel_instrumento1}</h5>
+              <h5>Instrumento 2: {this.state.usuarioPerfil.instrumento2}</h5> <h5>Nivel: {this.state.usuarioPerfil.nivel_instrumento2}</h5>
+             
               <h5>Generos</h5>
-              <Button color="danger">Eliminar usuario</Button>
+              <h5> {this.state.usuarioPerfil.genero_musico1}</h5>
+              <h5>{this.state.usuarioPerfil.genero_musico2}</h5>
+              <h5>Localización: {this.state.usuarioPerfil.localizacion}</h5>
+             
+              
+
+              <h5>Datos Adicionales:</h5>
+              {this.state.usuarioPerfil.textoMusico}
             </div>
             <div className="col-4"></div>
           </div>

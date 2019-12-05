6c7470a07fa1dbd66c2fb82540db3935004ce7f7
cacaolat123 <marcelmontes00@gmail.com>
2019-12-05 18:29:19 +0100

    cambios

--- src/PagTarjetas.jsx
+++ src/PagTarjetas.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import FiltrosMusico from "./FiltrosMusico.jsx";
 import Tarjetas from "./Tarjetas.jsx";
-import { APIURL,ELEMENTOS_POR_PAGINA } from "./Datos.js";
+import { APIURL, ELEMENTOS_POR_PAGINA } from "./Datos.js";
 
 class PagTarjetas extends React.Component {
   constructor(props) {
@@ -17,8 +17,6 @@ class PagTarjetas extends React.Component {
     this.cargaFiltro = this.cargaFiltro.bind(this);
     this.siguiente = this.siguiente.bind(this);
     this.anterior = this.anterior.bind(this);
-  
-   
   }
 
   anterior() {
@@ -29,7 +27,7 @@ class PagTarjetas extends React.Component {
   }
 
   componentDidMount() {
-    fetch(APIURL +"bandforall_musico/count")
+    fetch(APIURL + "bandforall_musico/count")
       .then(data => data.json())
       .then(resultado =>
         this.setState(
@@ -47,12 +45,14 @@ class PagTarjetas extends React.Component {
   }
 
   cargaDatos() {
-    let url;// = APIURL + `bandforall_musico?_size=${ELEMENTOS_POR_PAGINA}&_p=${this.state.pagina}`;
+    let url; // = APIURL + `bandforall_musico?_size=${ELEMENTOS_POR_PAGINA}&_p=${this.state.pagina}`;
 
-    if (this.state.afiltros.length==0 ) {
-      url = `${APIURL}bandforall_musico?_size=${ELEMENTOS_POR_PAGINA}&_p=${this.state.pagina}`; 
+    if (this.state.afiltros.length == 0) {
+      url = `${APIURL}bandforall_musico?_size=${ELEMENTOS_POR_PAGINA}&_p=${this.state.pagina}`;
     } else {
-      url = `${APIURL}bandforall_musico?_size=${ELEMENTOS_POR_PAGINA}&_p=${this.state.pagina}&_where=`+this.state.afiltros.join("~and");
+      url =
+        `${APIURL}bandforall_musico?_size=${ELEMENTOS_POR_PAGINA}&_p=${this.state.pagina}&_where=` +
+        this.state.afiltros.join("~and");
     }
 
     url = encodeURI(url);
@@ -66,44 +66,45 @@ class PagTarjetas extends React.Component {
       .catch(err => console.log(err));
   }
 
-  rangos(edad){
-    if (edad==="rango1"){
-      return [0,20];
+  rangos(edad) {
+    if (edad === "rango1") {
+      return [0, 20];
+    } else if (edad === "rango2") {
+      return [21, 30];
+    } else if (edad === "rango3") {
+      return [31, 40];
+    } else if (edad === "rango4") {
+      return [41, 99];
     }
-    else if(edad==="rango2"){
-      return [21,30];
-    }
-    else if(edad==="rango3"){
-      return [31,40];
-    }
-    else if(edad==="rango4"){
-      return [41,99];
-    }
-  
   }
 
   cargaFiltro(genero, instrumento, edad) {
-
     let url = "";
-    let afiltros=[];
+    let afiltros = [];
 
-    if (genero !== "Género"){
-      afiltros.push(`(genero_musico1,like,${genero}~)~or(genero_musico2,like,${genero}~)`);
+    if (genero !== "Género") {
+      afiltros.push(
+        `(genero_musico1,like,${genero}~)~or(genero_musico2,like,${genero}~)`
+      );
     }
-    if (instrumento !== "Instrumento"){
-      afiltros.push(`((instrumento1,like,${instrumento}~)~or(instrumento2,like,${instrumento}~)~or(instrumento3,like,${instrumento}~)~or(instrumento4,like,${instrumento}~)~or(instrumento5,like,${instrumento}~))`);
+    if (instrumento !== "Instrumento") {
+      afiltros.push(
+        `((instrumento1,like,${instrumento}~)~or(instrumento2,like,${instrumento}~)~or(instrumento3,like,${instrumento}~)~or(instrumento4,like,${instrumento}~)~or(instrumento5,like,${instrumento}~))`
+      );
     }
-    if (edad !== "Edad"){
+    if (edad !== "Edad") {
       let minmax = this.rangos(edad);
       afiltros.push(`((edad,gte,${minmax[0]})~and(edad,lte,${minmax[1]}))`);
     }
 
-    this.setState({afiltros});
+    this.setState({ afiltros });
 
-    if (afiltros.length==0 ) {
+    if (afiltros.length == 0) {
       url = `${APIURL}bandforall_musico?_size=${ELEMENTOS_POR_PAGINA}&_p=${this.state.pagina}`;
     } else {
-      url = `${APIURL}bandforall_musico?_size=${ELEMENTOS_POR_PAGINA}&_p=${this.state.pagina}&_where=`+afiltros.join("~and");
+      url =
+        `${APIURL}bandforall_musico?_size=${ELEMENTOS_POR_PAGINA}&_p=${this.state.pagina}&_where=` +
+        afiltros.join("~and");
     }
 
     url = encodeURI(url);
@@ -122,21 +123,23 @@ class PagTarjetas extends React.Component {
     if (this.state.musicos.length === null) {
       return <h4>Esperando datos...</h4>;
     }
-   
+
     let i = 1;
     let varTarjetas = this.state.musicos.map(el => (
-      <Tarjetas
-        key={i++}
-        nombre={el.nombre}
-        apellidos={el.apellidos}
-        edad={el.edad}
-        imagen={el.imagen}
-        texto={el.descripcion}
-        instrumento1={el.instrumento1}
-        instrumento2={el.instrumento2}
-        genero_musico1={el.genero_musico1}
-        genero_musico2={el.genero_musico2}
-      />
+      <div className="col-6">
+        <Tarjetas
+          key={i++}
+          nombre={el.nombre}
+          apellidos={el.apellidos}
+          edad={el.edad}
+          imagen={el.imagen}
+          texto={el.descripcion}
+          instrumento1={el.instrumento1}
+          instrumento2={el.instrumento2}
+          genero_musico1={el.genero_musico1}
+          genero_musico2={el.genero_musico2}
+        />
+      </div>
     ));
 
     return (
@@ -145,13 +148,28 @@ class PagTarjetas extends React.Component {
         <h1>Encuentra tu músico</h1>
         <br></br>
         <div className="container-fluid">
+
+
           <div className="row">
             <div className="col-2">
               <FiltrosMusico cargaFiltro={this.cargaFiltro} />
             </div>
             <div className="col-10">
+              
+              <div className="row">
               {varTarjetas}
-              <button
+              </div>
+              
+              
+              </div>
+          </div>
+
+
+
+          <div className="row">
+          <div className="col-3"></div>
+          <div className="col-9">
+          <button
                 disabled={this.state.pagina === 0}
                 className="btn btn-primary"
                 onClick={this.anterior}
@@ -165,7 +183,7 @@ class PagTarjetas extends React.Component {
               >
                 Siguiente
               </button>
-            </div>
+              </div>
           </div>
         </div>
       </>
